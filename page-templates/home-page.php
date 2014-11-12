a7ccfbe9ebeb3afdc2906bc9c7879decf53fb081
ipeinado <i.peinado.martinez@gmail.com>
2014-11-12 09:17:17 +0100

    first commit

---
+++ page-templates/home-page.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Template Name: IISG Home Page
+ * Description: Página principal de la página del Instituto de Investigación Sanitaria de Getafe
+ *
+ * @package WordPress
+ * @subpackage Twenty_Fourteen
+ * @since Twenty Fourteen 1.0
+ */
+
+get_header(); ?>
+
+<div id="main-content" class="main-content">
+
+	<div id="primary" class="content-area">
+		<div id="content" class="site-content" role="main">
+			<h1>Testing Home Page</h1>
+		</div><!-- #content -->
+	</div><!-- #primary -->
+</div><!-- #main-content -->
+
+<?php
+get_footer();
